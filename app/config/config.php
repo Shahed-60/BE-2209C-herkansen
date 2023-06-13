985e6eb0a3340c302d80faf3b0556c6b66c579b3
Shahed Amer <chanyeol6521354@gmail.com>
2023-06-13 12:42:52 +0200

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Mvc-2209c-P4-toets');
+// define('DB_NAME', 'mvc-oop-proeftoets');
+define('URLROOT', 'http://be-toets-2209c');
+
+
+// echo URLROOT;
+// echo DB_HOST;
