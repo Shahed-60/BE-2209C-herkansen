6bd9304bbb8bcbac425a1f1efb9334276c48e8df
Shahed Amer <chanyeol6521354@gmail.com>
2023-06-13 13:25:14 +0200

    config is weer aangepast naar de goede naam

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'Mvc-2209c-herkansen');
+define('DB_NAME', 'mvc-2209c-herkansen');
 // define('DB_NAME', 'mvc-oop-proeftoets');
 define('URLROOT', 'http://be-herkansen-mvc.com/');
 
