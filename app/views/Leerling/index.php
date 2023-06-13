c4b00114df5ab03897d84c0c05fa5eee3a1f5af3
Shahed Amer <chanyeol6521354@gmail.com>
2023-06-13 13:24:30 +0200

    de view is aangepast en hij doet het

---
+++ app/views/Leerling/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Leerling</title>
+</head>
+
+<body>
+    <u> <?= $data['title']; ?></u>
+
+    <table>
+        <thead>
+            <th>Naam</th>
+            <th>Mobiel</th>
+            <th>pakketnaam</th>
+            <th>Aantal Lessen</th>
+            <th>Prijs</th>
+            <th>Start datum</th>
+            <th>DatumRijbewijs behaald</th>
+
+        </thead>
+        <tbody>
+            <?= $data['tableRows']; ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
