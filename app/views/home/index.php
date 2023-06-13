d198c5a32de1e393d5cde4d2535d43e569ae16aa
Shahed Amer <chanyeol6521354@gmail.com>
2023-06-13 13:25:48 +0200

    de view is aangepast

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Leerlingen</title>
 </head>
 
 <body>
     <h3><?= $data['title']; ?></h3>
-    <a href="<?= URLROOT; ?>/Examinator/index">Overzicht Afgenomen Examnes Examinatoren</a>
+    <a href="<?= URLROOT; ?>/Leerling/index">Overzicht RijlesPakketten Leerlingen</a>
 
 
 
